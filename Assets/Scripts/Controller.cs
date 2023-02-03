bd1ade094b4815d7ff5b415db95d9642610f3e51
Siddhartha Pratap Singh <sidps1112@gmail.com>
2023-02-03 19:12:29 +0530

    Add ground detection

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -12,9 +12,48 @@ public class Controller : MonoBehaviour
 
     public float friction = 0.1f;
 
+    public bool isGroundDown = true;
+
+    public Vector2[] offset;
+
+    public float detectRange = 0.5f;
+
+    private bool isOnGround = true;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (isOnGround)
+        {
+            Debug.Log("Ground");
+        }
+        else
+        {
+            Debug.Log("Air");
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        foreach (Vector2 offseti in offset)
+        {
+            Debug.DrawRay(rb.position + offseti, Vector2.down * detectRange, Color.black, 0.1f);
+            RaycastHit2D hit = Physics2D.Raycast(rb.position + offseti, Vector2.down, detectRange, LayerMask.GetMask("Platform"));
+            if (hit.collider != null)
+            {
+                Debug.Log("A: " + hit.collider.gameObject.tag + " B:" + hit.collider.gameObject.name);
+                isOnGround = true;
+                break;
+            }
+            else
+            {
+                isOnGround = false;
+            }
+        }
     }
 
     public void Move(float horizontal)
