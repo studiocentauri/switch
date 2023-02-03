ea79ed0757e04f147c695698de423f783fa1d46d
Siddhartha Pratap Singh <sidps1112@gmail.com>
2023-02-03 18:00:56 +0530

    Add input manager

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public void Move(float horizontal)
+    {
+        Debug.Log("Move with: " + horizontal);
+    }
+}
